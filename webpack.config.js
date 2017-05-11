1c25cac162cb3889de15cdca7f9bcd1d21d38cbc
Lincoln Anderson <landerson@352inc.com>
2017-05-10 21:27:58 -0400

    Patch up webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,94 @@
 var path = require('path');
-var failPlugin = require('webpack-fail-plugin');
-var copyWebpackPlugin = require('copy-webpack-plugin');
 var webpack = require('webpack');
 
-var isProduction = process.env.NODE_ENV ? process.env.NODE_ENV.trim() == 'prod' : false;
+var copyPlugin = require('copy-webpack-plugin');
+var failPlugin = require('webpack-fail-plugin');
+var extractTextPlugin = require('extract-text-webpack-plugin');
 
-var config = {
-	entry : path.join(__dirname, 'src/js/index.js'),
-	output : {
-		filename: 'bundle.js',
-		path: path.join(__dirname, 'build'),
-		sourceMapFilename: 'bundle.map',
-	},
-	devServer : {
-		contentBase: path.join(__dirname, 'build'),
-		port: 3000,
-	},
-	module : {
-		rules :[
-			{
-	      test: /\.jsx?$/,
-	      exclude: /node_modules/,
-	      loader: 'babel-loader',
-	      options: {
-	        presets: ['es2015', 'stage-0', 'react']
-	      }
-	    },
-			{
-				test : /\.less$/,
-				loader: 'style-loader!css-loader!less-loader'
-			},
-			{
-				test: /\.(png|jpg|svg)$/,
-				loader: ['url-loader', 'image-webpack-loader']
-			}
+function config(env) {
+
+	var isProduction = (env === 'prod');
+
+	console.log('env', env);
+	console.log('isProduction', isProduction);
+
+	var lessRule = isProduction ?
+		{
+			test : /\.less$/,
+			loader : extractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: 'css-loader!less-loader'
+			}),
+		} : {
+			test : /\.less$/,
+			loader: 'style-loader!css-loader!less-loader'
+		};
+
+	var config = {
+		entry : path.join(__dirname, 'src/js/index.js'),
+		output : {
+			filename: 'bundle.js',
+			path: path.join(__dirname, 'build'),
+		},
+		devServer : {
+			contentBase: path.join(__dirname, 'build'),
+			port: 3000,
+		},
+		module : {
+			rules :[
+				{
+		      test: /\.jsx?$/,
+		      exclude: /node_modules/,
+		      loader: 'babel-loader',
+		      options: {
+		        presets: isProduction ?
+							['es2015', 'stage-0', 'react']
+							: ['es2015', 'stage-0', 'react', 'react-hmre']
+		      }
+		    },
+				{
+					test: /\.(png|jpg|svg)$/,
+					loader: ['url-loader', 'image-webpack-loader']
+				},
+				lessRule
+			]
+		},
+		resolve: {
+	    extensions: ['.js', '.less']
+		},
+		plugins : [
+			new webpack.ProvidePlugin({
+	        PropTypes: 'prop-types',
+	        React: 'react',
+	        Component: ['react', 'Component'],
+	        cn: 'classnames',
+	        get: ['lodash', 'get'],
+	        map: ['lodash', 'map'],
+	        assign: ['lodash', 'assign'],
+	        Link: ['react-router', 'Link'],
+	        Button: ['semantic-ui-react', 'Button'],
+	        Icon: ['semantic-ui-react', 'Icon'],
+	    }),
+			failPlugin,
+			new copyPlugin([{
+				from: path.join(__dirname, 'src/html'),
+				to: path.join(__dirname, 'build')
+			}])
 		]
-	},
-	resolve: {
-    extensions: ['.js', '.less']
-	},
-	plugins : [
-		new webpack.ProvidePlugin({
-        PropTypes: 'prop-types',
-        React: 'react',
-        Component: ['react', 'Component'],
-        cn: 'classnames',
-        get: ['lodash', 'get'],
-        map: ['lodash', 'map'],
-        assign: ['lodash', 'assign'],
-        Link: ['react-router', 'Link'],
-        Button: ['semantic-ui-react', 'Button'],
-        Icon: ['semantic-ui-react', 'Icon'],
-    }),
-		failPlugin,
-		new copyWebpackPlugin([{
-			from: path.join(__dirname, 'src/html'),
-			to: path.join(__dirname, 'build')
-		}])
-	]
-};
+	};
+
+	if (isProduction) {
+		config.plugins.push(
+			new webpack.optimize.UglifyJsPlugin({
+		    compress: {
+		    	warnings: false
+		    }
+			})
+		);
+		config.plugins.push(new extractTextPlugin('bundle.css'));
+	}
+
+	return config;
+}
 
 module.exports = config;
